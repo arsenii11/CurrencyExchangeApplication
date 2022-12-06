84df79342e9d36c2d644d36d049fdcbca4b7557a
Arsenii Dragunkin <57676564+arsenii11@users.noreply.github.com>
2022-12-06 16:54:22 +0200

    "Network Statuses"

--- app/src/main/java/com/example/conferoapplication/service/CurrenciesService.kt
+++ app/src/main/java/com/example/conferoapplication/service/CurrenciesService.kt
@@ -1,11 +1,15 @@
 package com.example.conferoapplication.service
 
+import com.example.conferoapplication.Utilities.Links
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 
 interface CurrenciesService {
 
     @GET("currency/list")
-    suspend fun getSupportedCurrencies(): Map<String, String>
+    suspend fun getSupportedCurrencies(
+        @Query("api_key") access_key: String = Links.API_KEY,
+    ): Map<String, String>
 
 }
