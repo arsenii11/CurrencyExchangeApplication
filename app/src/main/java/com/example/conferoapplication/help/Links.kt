48014854ea4258cc0dca9c60878fcb4d1b0d7fc5
Arsenii Dragunkin <57676564+arsenii11@users.noreply.github.com>
2022-12-06 02:32:23 +0200

    "added  to gradle jsonSupport"

---
+++ app/src/main/java/com/example/conferoapplication/help/Links.kt
@@ -0,0 +1,23 @@
+package com.example.conferoapplication.help
+
+/**
+ * All URL listed in this class
+ */
+
+class Links {
+
+    companion object {
+
+        //Base URL
+
+        const val BASE_URL = "https://api.getgeoapi.com/api/v2/currency/"
+
+        //API KEY
+        const val API_KEY = "abce412468f5b2d4eea39485826cef2a8cb39903"
+
+        //COVERT URL
+        const val  CONVERT_URL = "convert"
+
+    }
+
+}
--- app/src/main/java/com/example/conferoapplication/model/Currency.kt
+++ app/src/main/java/com/example/conferoapplication/model/Currency.kt
@@ -1,3 +1,12 @@
 package com.example.conferoapplication.model
 
 data class Currency(val symbol: String, val displayName: String)
+
+data class ApiResponse(
+    val amount: String,
+    val base_currency_code: String,
+    val base_currency_name: String,
+    var rates: HashMap<String, Rates> = HashMap(),
+    val status: String,
+    val updated_date: String
+)
