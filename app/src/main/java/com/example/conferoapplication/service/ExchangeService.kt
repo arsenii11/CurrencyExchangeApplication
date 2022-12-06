54e81bfa58fd8cf6688ff8f9c6d4f6fe77422ec9
Arsenii Dragunkin <57676564+arsenii11@users.noreply.github.com>
2022-12-06 02:32:23 +0200

    "added  to gradle jsonSupport"

--- app/src/main/java/com/example/conferoapplication/service/ExchangeService.kt
+++ app/src/main/java/com/example/conferoapplication/service/ExchangeService.kt
@@ -1,11 +1,27 @@
 package com.example.conferoapplication.service
 
+import com.example.conferoapplication.help.Links
+import com.example.conferoapplication.model.ApiResponse
+import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 
 interface ExchangeService {
 
     @GET("currency/list")
     suspend fun getSupportedCurrencies(): Map<String, String>
+
+}
+
+interface ApiService {
+
+    @GET(Links.CONVERT_URL)
+    suspend fun convertCurrency(
+        @Query("api_key") access_key: String,
+        @Query("from") from: String,
+        @Query("to") to: String,
+        @Query("amount") amount: Double
+    ) : Response<ApiResponse>
 
 }
