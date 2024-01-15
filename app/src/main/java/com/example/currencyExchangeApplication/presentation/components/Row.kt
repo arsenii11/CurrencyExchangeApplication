23e417fa66d9bd82cd706288bb68cdaf49f8ca5b
arseniidragunkin <arsenii.dragunkin@mintos.com>
2024-01-15 11:26:56 +0100

    fix

--- app/src/main/java/com/example/currencyExchangeApplication/presentation/components/Row.kt
+++ app/src/main/java/com/example/currencyExchangeApplication/presentation/components/Row.kt
@@ -4,8 +4,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
+/*import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp*/
 
 
 @Composable
@@ -15,11 +15,11 @@ fun Row(
     onValueChange: (String) -> Unit,*/
 ) {
 
-    Column(
+   /* Column(
         modifier = Modifier
             .fillMaxSize()
             .padding(16.dp)
-    ){}}
+    ){}*/}
 /*{
         // Representing the LinearLayout
         Column(
