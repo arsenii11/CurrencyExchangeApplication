2eb9424f4dece4df20248ab9f0d14a5eb24edc04
arseniidragunkin <arsenii.dragunkin@mintos.com>
2024-01-15 13:11:57 +0100

    Working compose

--- app/src/main/java/com/example/currencyExchangeApplication/presentation/components/Row.kt
+++ app/src/main/java/com/example/currencyExchangeApplication/presentation/components/Row.kt
@@ -1,110 +1,125 @@
 package com.example.currencyExchangeApplication.presentation.components
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.DropdownMenu
+import androidx.compose.material.DropdownMenuItem
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.runtime.Composable
-/*import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp*/
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 
 @Composable
-fun Row(
-  /*  amount: Double,
+fun CurrencyRow(
+    amount: Double,
     currencyList: Array<String>,
-    onValueChange: (String) -> Unit,*/
+    onValueChange: (String) -> Unit,
 ) {
+    var selectedCurrency by remember { mutableStateOf("EUR") }
+    var textValue by remember { mutableStateOf("") }
 
-   /* Column(
+    Row(
         modifier = Modifier
             .fillMaxSize()
             .padding(16.dp)
-    ){}*/}
-/*{
-        // Representing the LinearLayout
-        Column(
+    ) {
+        // Dropdown
+        Box(
             modifier = Modifier
-                .fillMaxWidth()
-                .height(55.dp)
+                .weight(1f)
+                .background(MaterialTheme.colors.background)
+                .padding(vertical = 8.dp),
+            contentAlignment = Alignment.CenterStart
         ) {
-            Text(
-                text = "EUR",
-                modifier = Modifier
-                    .padding(start = 10.dp)
-                    .weight(1f),
-                style = typography.caption
-            )
-
-            var expanded by remember { mutableStateOf(false) }
-            var selectedIndex by remember { mutableStateOf(0) }
-
-            *//*
-*/
-/*OutlinedTextField(
-                value = amount,
-                onValueChange = {
-                    amount = onValueChange
-                },
-                label = {
-                    Text(text = "Amount")
-                },
-                keyboardOptions = KeyboardOptions.Default.copy(
-                    keyboardType = KeyboardType.Number,
-                    capitalization = KeyboardCapitalization.None
-                ),
-                modifier = Modifier
-                    .fillMaxHeight()
-                    .padding(start = 8.dp)
-                    .weight(1f)
-            )*//*
-*/
-/*
-
-
             DropdownMenu(
-                expanded = expanded,
-                onDismissRequest = { expanded = false },
-                modifier = Modifier
-                    .fillMaxHeight()
-                    .width(60.dp)
+                expanded = false,
+                onDismissRequest = { },
+                modifier = Modifier.fillMaxWidth()
             ) {
-                listOf("Euro", "USD", "GBP").forEachIndexed { index, item ->
-                    DropdownMenuItem(
-                        onClick = {
-                            selectedIndex = index
-                            expanded = false
-                        },
-                        modifier = Modifier.fillMaxWidth()
-                    ) {
-
-                    }
+                DropdownMenuItem(onClick = { selectedCurrency = "EUR" }) {
+                    Text("EUR")
+                }
+                DropdownMenuItem(onClick = { selectedCurrency = "USD" }) {
+                    Text("USD")
+                }
+                DropdownMenuItem(onClick = { selectedCurrency = "RUB" }) {
+                    Text("RUB")
                 }
             }
-
-            Box(
+            Row(
                 modifier = Modifier
-                    .fillMaxHeight()
-                    .width(24.dp),
-                contentAlignment = Alignment.Center
+                    .fillMaxWidth()
+                    .clickable { },
+                horizontalArrangement = Arrangement.SpaceBetween
             ) {
+                Text(
+                    text = selectedCurrency,
+                    style = MaterialTheme.typography.body1,
+                    color = Color.White,
+                    modifier = Modifier.padding(16.dp)
+                )
                 Icon(
                     imageVector = Icons.Default.ArrowDropDown,
-                    contentDescription = "DropDown",
+                    contentDescription = "Dropdown Icon",
                     modifier = Modifier
-                        .clickable { expanded = true }
-                        .rotate(if (expanded) 180f else 0f)
-                        .padding(4.dp)
+                        .size(24.dp)
+                        .padding(16.dp)
                 )
             }
         }
-    }*//*
 
+        // OutlinedTextField
+        OutlinedTextField(
+            value = textValue,
+            onValueChange = {
+                textValue = it
+            },
+            label = { Text("Amount") },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(
+                capitalization = KeyboardCapitalization.None,
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Done
+            ),
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxWidth()
+                .padding(start = 8.dp),
+        )
+    }
 }
+
+
 
 @Preview(showBackground = true)
 @Composable
 fun PreviewMainScreen() {
-    Row(
+    CurrencyRow(
         12.0, onValueChange = {}, currencyList = arrayOf(
             "EUR",
             "USD",
@@ -125,4 +140,4 @@ fun PreviewMainScreen() {
             "SOS"
         )
     )
-}*/
+}
